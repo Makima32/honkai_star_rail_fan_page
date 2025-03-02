efb1acd19182e14a6cc9feb51ddcf69e25ba5a3b
Makima32 <omartorresyuste@gmail.com>
2025-03-02 20:21:46 +0000

    Real 1.0 version

--- src/Components/Slider_images/Slider_images.jsx
+++ src/Components/Slider_images/Slider_images.jsx
@@ -2,7 +2,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-function Slider_images() {
+import "./Slider_images.css"
+
+function Slider_images( {img1, img2, img3} ) {
   const settings = {
     dots: true,
     infinite: true,
@@ -14,14 +16,25 @@ function Slider_images() {
   return (
     <div className="Slider_div_father"> 
 
+
+    <div className= "Slider_div_text">
+
+    <h2>Descubre los Mundos de Honkai Star Rail</h2>
+
+    <p>A bordo del Expreso Astral, viajarás a través de diferentes mundos, cada uno con su propia historia, habitantes y desafíos únicos. Desde ciudades futuristas hasta ruinas ancestrales, cada destino esconde secretos y aventuras por descubrir. Prepárate para explorar, luchar y desentrañar los misterios del universo.</p>
+    </div>
+
+    <div className="Slider_div_carrusel">
+
     <Slider {...settings}>
 
-      <div><img src="public\img\aglaea_Splash_art.png" alt="Slide 1" /></div>
-      <div><img src="public\img\archeron_Splash_art2.png" alt="Slide 2" /></div>
-      <div><img src="public\img\galaxy-honkai-star-rail.png" alt="Slide 3" /></div>
+      <div><img src={img1}alt="Slide 1" /></div>
+      <div><img src={img2} alt="Slide 2" /></div>
+      <div><img src={img3} alt="Slide 3" /></div>
 
     </Slider>
 
+    </div>
     </div>
 
   );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Footer from "../../Components/Footer/Footer";
 import Header_home from "../../Components/Header_home/Header_home";
 import Honkai_description from "../../Components/Honkai_description/Honkai_description";
 import Honkai_trailer from "../../Components/HonkaiTrailer/HonkaiTrailer";
+import Slider_images from "../../Components/Slider_images/Slider_images";
 
 function Home_Page() {
 
@@ -21,6 +22,12 @@ function Home_Page() {
             <Honkai_trailer />
             <hr></hr>
             <CharacterList></CharacterList>
+           
+            <Slider_images 
+            img1="public\img\slider_image1.webp"
+            img2="public\img\slider_image2.png"
+            img3="public\img\slider_image3.png"
+/>
             <Footer></Footer>
         </>
     ]
